f93cb5be1693d0d6f5e0ed1c3509400999c74eea
mangel <mangel@mangel-Latitude-E5410>
2018-02-08 11:19:44 +0100

    Prueba de Commit

--- EjemploparaDAD/src/main/java/es/urjc/dad/practica/prueba/Controlador.java
+++ EjemploparaDAD/src/main/java/es/urjc/dad/practica/prueba/Controlador.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class Controlador  {
 	@RequestMapping ("/main")
+	//Romo es gay
 	
 	public String contWeb (Model model){		
 		
