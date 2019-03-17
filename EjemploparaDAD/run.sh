4fcf04adb1ad0f355f34e1fd6be46b1cb8df11bf
Héctor Hernández Marqués <H.Hernandez@alumnos.upm.es>
2019-03-17 01:40:11 +0100

    xd

--- EjemploparaDAD/run.sh
+++ EjemploparaDAD/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+while ! nc -z hectorh-mysql-service 3306 ; do
+    echo "Waiting for MySQL server to be available"
+    sleep 2
+done
 
 exec java -jar -Dspring.datasource.url="jdbc:mysql://hectorh-mysql-service:3306/test" ejem1-0.0.1-SNAPSHOT.jar
