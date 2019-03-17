b5013f762128654f8b35255e1ff4503e382b6982
Héctor Hernández Marqués <H.Hernandez@alumnos.upm.es>
2019-03-17 02:31:10 +0100

    Revert "xd"
    This reverts commit 5d40cad57c5b61485d746c8150bdbe73a74bbace.

--- EjemploparaDAD/src/main/java/es/urjc/dad/practica/prueba/DatabaseUsersLoader.java
+++ EjemploparaDAD/src/main/java/es/urjc/dad/practica/prueba/DatabaseUsersLoader.java
@@ -16,11 +16,11 @@ import es.urjc.dad.practica.prueba.Repositories.UsersRepository;
  @PostConstruct
  	private void initDatabase() {
 
-	 //userRepository.save(new User("MiguelAngel","miguel5691@gmail","vino","ROLE_USER"));
-	 //userRepository.save(new User("Dani","miguel5692@gmail.com","vino","ROLE_USER"));
-	 //userRepository.save(new User("User","miguel5693@gmail","vino","ROLE_USER"));
-	 //userRepository.save(new User("Admin","miguel5694@gmail","patatas","ROLE_USER","ROLE_ADMIN"));
-	 //userRepository.save(new User("Romo","miguel5695@gmail","patatas","ROLE_USER","ROLE_ADMIN"));
+	 userRepository.save(new User("MiguelAngel","miguel5691@gmail","vino","ROLE_USER"));
+	 userRepository.save(new User("Dani","miguel5692@gmail.com","vino","ROLE_USER"));
+	 userRepository.save(new User("User","miguel5693@gmail","vino","ROLE_USER"));
+	 userRepository.save(new User("Admin","miguel5694@gmail","patatas","ROLE_USER","ROLE_ADMIN"));
+	 userRepository.save(new User("Romo","miguel5695@gmail","patatas","ROLE_USER","ROLE_ADMIN"));
 	 }
 }
 
